87bfe04203e11d1654fe9570ccd82713ce361ca2
prateek3189 <prateek.magarde@gmail.com>
2026-04-28 22:16:51 +0530

    Angular - Lazy Loading

---
+++ Udemy-Angular-The-Complete-Guide-2025/15-lazy-loading/01-starting-project/src/app/users/users.routes.ts
@@ -0,0 +1,36 @@
+import { Routes } from '@angular/router';
+
+import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
+import {
+  NewTaskComponent,
+  canLeaveEditPage,
+} from '../tasks/new-task/new-task.component';
+import { TasksService } from '../tasks/tasks.service';
+
+export const routes: Routes = [
+  {
+    path: '',
+    providers: [TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks',
+        loadComponent: () =>
+          import('../tasks/tasks.component').then((mod) => mod.TasksComponent),
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage],
+      },
+    ],
+  },
+];
